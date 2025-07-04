56b5040614cf6131ca57a414c86e508f14c58ac0
Ali <aliuajibade317@gmail.com>
2025-07-04 21:34:18 +0100

    add slug

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import type { JSX } from "react";
 import { blogData } from "@/app/data";
 import styles from "./page.module.css";
 import Image from "next/image";
@@ -8,19 +10,32 @@ interface BlogDetailProps {
   };
   searchParams?: { [key: string]: string | string[] | undefined };
 }
-const BlogDetail = ({ params }: BlogDetailProps) => {
+
+const BlogDetail = ({ params }: BlogDetailProps): JSX.Element => {
   const item = blogData.find((i) => i.slug === params.slug);
+
   if (!item) {
     return <div className={styles.detail}>Blog post not found.</div>;
   }
+
   return (
     <div className={styles.detail}>
       <h1>{item.title}</h1>
-      <Image src={item.image} alt={item.title} className={styles.image} width={400} height={400}/>
+      <Image
+        src={item.image}
+        alt={item.title}
+        className={styles.image}
+        width={400}
+        height={400}
+        priority
+      />
       {item.content.split("\n\n").map((para, index) => (
-        <p className={styles.p} key={index}>{para}</p>
+        <p className={styles.p} key={index}>
+          {para}
+        </p>
       ))}
     </div>
   );
-}
-export default BlogDetail
+};
+
+export default BlogDetail;
