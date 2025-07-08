870fe94ba73d732736ac827f5b6472886022e09f
Ali <aliuajibade317@gmail.com>
2025-07-08 14:49:03 +0100

    add server

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const { createServer } = require("http");
-const next = require("next");
 const { parse } = require("url");
+const next = require("next");
+
+const app = next({ dev: false });
+const handle = app.getRequestHandler();
 
 const port = process.env.PORT || 3000;
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+  }).listen(port, () => {
+    console.log(`> Ready on port ${port}`);
   });
 });
