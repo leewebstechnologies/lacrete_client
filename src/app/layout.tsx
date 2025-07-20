800acef860106346b1582dfa1afca3dcdcd4a232
Ali <aliuajibade317@gmail.com>
2025-07-20 01:31:53 +0100

    Return Bootstrap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import BootstrapClient from "./components/BootstrapClient";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
@@ -38,6 +41,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Navbar />
         {children}
+        <BootstrapClient />
         <Footer />
       </body>
     </html>
